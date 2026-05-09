d77c4c8910f9264e15e49ab2e12e846c839ec91f
_niranjanv_ <niranjanv2k4@gmail.com>
2026-05-09 12:57:08 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  //Logic
+
+  const name = "niranjan";
+
+  return (
+
+    //UI
+    <div>
+      <h1>My first react app</h1>
+      <p>Hello {name}</p>
+    </div>
+  )
+}
+
+export default App
