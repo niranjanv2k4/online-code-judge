9b0a4088203c6585ebfeef8d4706bbaad3f8380c
_niranjanv_ <niranjanv2k4@gmail.com>
2026-05-09 13:09:14 +0530

    what is the use of useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 
 function App() {
 
   //Logic
-
+  const [ count, setCount ] = useState(0);
   const name = "niranjan";
+  var normal_count = 0;
 
   return (
 
@@ -11,6 +13,9 @@ function App() {
     <div>
       <h1>My first react app</h1>
       <p>Hello {name}</p>
+      <h2>COUNT: {count}  {normal_count}</h2>
+      <button onClick={() => { setCount(count + 1); normal_count++} }>increment</button>
+      <button onClick={() => { setCount(count - 1); normal_count--} }>decrement</button>
     </div>
   )
 }
